fb962802b663681642dbae127fd0d0f09f501efc
xzq.xu <1325732643@qq.com>
2025-02-10 22:29:37 +0800

    刚开始学习没有进入状态

---
+++ day1/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 10;
+        new Main().fun1(a);
+        System.out.println(a);
+    }
+
+    //每个方法由自己的栈空间
+    public void fun1(int a){
+        int b = 10;
+        Person person = new Person();
+        p.id = 1;
+        p.name = "asd";
+        a = a + b;
+        //此时a = 20 
+        System.out.println(a);
+        //但是不影响方法结束时 外部的a依然是10
+    }
+
+
+}
+
+
+class Person {
+    int id;
+    String name;
+}
