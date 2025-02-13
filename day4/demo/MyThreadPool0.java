d2b7d6922584e29d85a914780ec1e33d8914a534
xzq.xu <1325732643@qq.com>
2025-02-14 00:55:18 +0800

    2、我需要一个容器来存放这个task，以便于调用

--- day4/demo/MyThreadPool0.java
+++ day4/demo/MyThreadPool0.java
@@ -1,5 +1,8 @@
 package day4.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 定义核心组件：任务队列、工作线程、线程池状态管理等。
     实现任务提交方法（execute）。
@@ -11,9 +14,18 @@ package day4.demo;
 
 public class MyThreadPool0 {
     
-    //1.我需要一个线程来执行task任务
+    //1、我需要一个线程来执行task任务
     // 这样可以另起一个线程调度这个task了但是，这个线程无法复用
+
+    //2、我需要一个容器来存放这个task，以便于调用
+    private List<Runnable> taskList = new ArrayList<>();
+
+    //3、假设线程池只有一个线程，怎么让这个线程可以复用
+    
+
     public void execute(Runnable task){
-        new Thread(task).start();
+        taskList.add(task);
+        taskList.forEach(e -> new Thread(e).start());
+        // new Thread(task).start();
     }
 }
